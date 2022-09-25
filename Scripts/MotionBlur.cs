74ea2114b31fd77758920e7f52e5e38b6db4eca9
Dumble009 <daichi19990821@gmail.com>
2022-09-25 09:55:53 +0900

    feat: カメラを前後に動かす処理をMotionBlur.csに実装。キー操作で前後に移動できることを確認

--- Scripts/MotionBlur.cs
+++ Scripts/MotionBlur.cs
@@ -3,6 +3,14 @@
 public class MotionBlur : MonoBehaviour
 {
     [SerializeField] Material motionBlurMaterial;
+    [SerializeField] float speed;
+
+    private void Update()
+    {
+        // 十字キーの上下 or W,Sキーで前後に移動する処理
+        float vertical = Input.GetAxis("Vertical");
+        transform.position += Vector3.forward * vertical * speed * Time.deltaTime;
+    }
 
     private void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
