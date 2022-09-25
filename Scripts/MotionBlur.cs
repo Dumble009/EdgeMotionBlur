af61e877e17a5d7d4712990660fdc5f0282c6b5f
Dumble009 <daichi19990821@gmail.com>
2022-09-25 09:53:21 +0900

    feat: MotionBlur.csにPostEffectを適用する処理を実装し、MotionBlurSampleSceneのMainCameraにアタッチした。デフォルトのネガポジ変換のエフェクトが適用されている事を確認

--- Scripts/MotionBlur.cs
+++ Scripts/MotionBlur.cs
@@ -1,18 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MotionBlur : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Material motionBlurMaterial;
 
-    // Update is called once per frame
-    void Update()
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
-        
+        Graphics.Blit(src, dest, motionBlurMaterial);
     }
 }
